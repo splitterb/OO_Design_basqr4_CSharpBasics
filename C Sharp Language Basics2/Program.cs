4806bbdfacf3866419ef9c42599332c656d4d2c7
brandonsplitter <basqr4@mail.missouri.edu>
2018-03-14 22:03:20 -0500

    Adding c sharp

---
+++ C Sharp Language Basics2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Language_Basics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const byte sample1 = 0x3A;
+            byte sample2 = 58;
+            int heartRate = 85;
+            double deposits = 135002796;
+            const float acceleration = 9.800F;
+            double mass = 14.6;
+            double distance = 129.763001;
+            bool lost = true;
+            bool expensive = true;
+            int choice = 2;
+            const char integral = '\u222B';
+            const String greeting = "Hello";
+            String name = "Karen";
+
+            if(sample1 == sample2)
+            {
+                Console.WriteLine("The samples are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The samples are not equal.");
+            }
+
+            if(heartRate >= 40 && heartRate <= 80)
+            {
+                Console.WriteLine("Heart rate is normal.");
+            }
+            else
+            {
+                Console.WriteLine("Heart rate is not normal.");
+            }
+
+            if(deposits >= 100000000)
+            {
+                Console.WriteLine("You are exceedingly wealthy.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry you are so poor.");
+            }
+
+            double force = mass * acceleration;
+
+            Console.Write("force= " + force + "\n");
+
+            Console.Write(distance + " is the distance.\n");
+
+            if(lost == true && expensive == true)
+            {
+                Console.WriteLine("I am really sorry! I will get the manager.");
+            }
+            else if (lost == true && expensive == false)
+            {
+                Console.WriteLine("Here is coupon for 10% off.");
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("You chose 1.");
+                    break;
+                case 2:
+                    Console.WriteLine("You chose 2.");
+                    break;
+                case 3:
+                    Console.WriteLine("You chose 3.");
+                    break;
+                default:
+                    Console.WriteLine("You made an unknown choice.");
+                    break;
+            }
+
+            Console.Write(integral + " is an integral.\n");
+
+            for(int i = 5; i <= 10; i++)
+            {
+                Console.Write("i = " + i + "\n");
+            }
+
+            int age = 0;
+            while (age < 6)
+            {
+                Console.Write("age = " + age + "\n");
+                age++;
+            }
+
+            Console.Write(greeting + " " + name + "\n");
+        }
+    }
+}
